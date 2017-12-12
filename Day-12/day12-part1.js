c8a5e4119568965496d788a17dd7c31dcfead08d
Kristóf Koncz <konczkristof93@gmail.com>
2017-12-12 19:38:34 +0100

    Day 12

---
+++ Day-12/day12-part1.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('input.txt', 'utf8');
+const lines = file.split("\n");
+
+const programs = [];
+for (line of lines) {
+  const items = line.split(' <-> ');
+  const id = parseInt(items[0], 10)
+
+  programs[id] = {
+    id: id,
+    pipes: items[1].split(', ').map(x => parseInt(x, 10)),
+    visited: false
+  };
+}
+
+const queue = [0];
+while (queue.length) {
+  const id = queue.shift();
+  const program = programs[id];
+
+  program.pipes.forEach(p => {
+    if (!programs[p].visited) {
+      queue.push(p);
+    }
+  })
+  
+  program.visited = true;
+}
+
+const count = programs.filter(p => p.visited).length;
+console.log(count);
---
+++ Day-12/day12-part2.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('input.txt', 'utf8');
+const lines = file.split("\n");
+
+const programs = [];
+for (line of lines) {
+  const items = line.split(' <-> ');
+  const id = parseInt(items[0], 10)
+
+  programs[id] = {
+    id: id,
+    pipes: items[1].split(', ').map(x => parseInt(x, 10)),
+    visited: false
+  };
+}
+
+function bfs(startId) {
+  const queue = [startId];
+
+  while (queue.length) {
+    const id = queue.shift();
+    const program = programs[id];
+
+    program.pipes.forEach(p => {
+      if (!programs[p].visited) {
+        queue.push(p);
+      }
+    })
+    
+    program.visited = true;
+  }
+}
+
+let count = 0;
+while (!programs.every(p => p.visited)) {
+  const start = programs.find(p => !p.visited);
+  bfs(start.id);
+  count++;
+}
+
+console.log(count);
